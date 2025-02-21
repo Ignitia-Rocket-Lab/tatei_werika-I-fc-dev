27caada47c660b3827e7d0cb77e6e524fff98acb
David-OC17 <daveoc01@icloud.com>
2025-02-21 13:19:46 -0600

    fix: pinout configuration

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,20 +57,52 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_13
-#define B1_GPIO_Port GPIOC
-#define USART_TX_Pin GPIO_PIN_2
-#define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
-#define USART_RX_GPIO_Port GPIOA
-#define LD2_Pin GPIO_PIN_5
-#define LD2_GPIO_Port GPIOA
+#define BUILTIN_BLUE_LED_Pin GPIO_PIN_13
+#define BUILTIN_BLUE_LED_GPIO_Port GPIOC
+#define TPU_CS_Pin GPIO_PIN_2
+#define TPU_CS_GPIO_Port GPIOC
+#define TPU_MOSI_Pin GPIO_PIN_3
+#define TPU_MOSI_GPIO_Port GPIOC
+#define BATTERY_VOLT_Pin GPIO_PIN_1
+#define BATTERY_VOLT_GPIO_Port GPIOA
+#define DEBUG_TX_Pin GPIO_PIN_2
+#define DEBUG_TX_GPIO_Port GPIOA
+#define DEBUG_RX_Pin GPIO_PIN_3
+#define DEBUG_RX_GPIO_Port GPIOA
+#define BUILTIN_GREEN_LED_Pin GPIO_PIN_5
+#define BUILTIN_GREEN_LED_GPIO_Port GPIOA
+#define TPU_SCK_Pin GPIO_PIN_10
+#define TPU_SCK_GPIO_Port GPIOB
+#define BAR_SCK_Pin GPIO_PIN_12
+#define BAR_SCK_GPIO_Port GPIOB
+#define BAR_CS_Pin GPIO_PIN_13
+#define BAR_CS_GPIO_Port GPIOB
+#define PYRO1_Pin GPIO_PIN_8
+#define PYRO1_GPIO_Port GPIOC
+#define PYRO0_Pin GPIO_PIN_9
+#define PYRO0_GPIO_Port GPIOC
+#define EXT_LED0_Pin GPIO_PIN_9
+#define EXT_LED0_GPIO_Port GPIOA
+#define EXT_LED1_Pin GPIO_PIN_10
+#define EXT_LED1_GPIO_Port GPIOA
+#define EXT_LED2_Pin GPIO_PIN_11
+#define EXT_LED2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
+#define BAR_MISO_Pin GPIO_PIN_11
+#define BAR_MISO_GPIO_Port GPIOC
+#define BAR_MOSI_Pin GPIO_PIN_12
+#define BAR_MOSI_GPIO_Port GPIOC
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+#define IMU_RST_Pin GPIO_PIN_5
+#define IMU_RST_GPIO_Port GPIOB
+#define IMU_SCL_Pin GPIO_PIN_6
+#define IMU_SCL_GPIO_Port GPIOB
+#define IMU_SDA_Pin GPIO_PIN_7
+#define IMU_SDA_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
