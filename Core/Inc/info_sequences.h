ba1ef1a4fe7bf84fdce765d023f373cd5fec4160
David-OC17 <daveoc01@icloud.com>
2025-02-22 14:55:04 -0600

    feat: new info sequences library

---
+++ Core/Inc/info_sequences.h
@@ -0,0 +1,25 @@
+#ifndef info_sequences_h
+#define info_sequences_h
+
+#include "stm32f4xx_hal.h"
+
+#include <stdio.h>
+
+#define LED_BLINK_TIME_SEC 3
+#define LED_BLINK_FREQ_HZ 0.5
+
+typedef enum {
+  LED_OFF = 0,
+  LED_RED,
+  LED_GREEN,
+  LED_BLUE,
+  LED_YELLOW,
+  LED_CYAN,
+  LED_MAGENTA,
+  LED_WHITE
+} LED_COLOR;
+
+void blink_builtin_LED(LED_COLOR color);
+
+
+#endif // INFO_SEQUENCES_H
