ebd3bfe99de0905f1bebf2f285c32bb6a434ac37
David-OC17 <daveoc01@icloud.com>
2025-02-22 17:46:10 -0600

    fix: running Unity tests

--- Core/Inc/testing.h
+++ Core/Inc/testing.h
@@ -1,15 +1,17 @@
 #ifndef TESTING_H
 #define TESTING_H
 
+#include <stdint.h>
+
 #include "main.h"
 #include "stm32f4xx_hal.h"
 #include "usart.h"
 
 #include "unity.h"
 
-#include <stdint.h>
-
 #define RUN_SUCCESS_TESTS
+
+void test_main(void);
 
 __weak void SystemClock_Config(void);
 __weak void MX_I2C1_Init(void);
@@ -17,34 +19,11 @@ __weak void MX_I2C1_Init(void);
 // Redefine fputc to output to a specific UART channel called TEST_RX/TX instead
 // to whatever it would default to.
 
-#if defined(TEST_TX_GPIO_Port) && defined(TEST_TX_Pin)
-void UnityOutputChar(int c) {
-  char ch = (char)c;
-  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-}
-
-void UnityOutputStart(void) {}
-void UnityOutputComplete(void) {}
-
-void setUp(void) {}
-void tearDown(void) {}
-#else
-#error                                                                         \
-    "Either TEST_TX_Pin or TEST_TX_GPIO_Port not defined, cannot print test outputs. Probably not compiling in test mode.
-#endif
-
-#if defined(RUN_SUCCESS_TESTS) || defined(RUN_FAILURE_TESTS)
-int test_success(void);
-int test_failure(void);
-#else
-#error                                                                         \
-    "Define either RUN_SUCCESS_TESTS or RUN_FAILURE_TESTS in your compiler settings."
-#endif
+void custom_putchar(int c);
 
 /**
  * Testing BNO055 IMU library.
  */
-#include "BNO055.h"
 
 void test_bno055_read_acc_x_success(void);
 void test_bno055_read_acc_y_success(void);
